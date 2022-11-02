222d29796ea421a29621dd87935c1648fc02899f
mgedikoglu <mustafaenes2012@gmail.com>
2022-11-03 02:37:30 +0300

    deploying

--- controller/mongoDB.controller.js
+++ controller/mongoDB.controller.js
@@ -28,9 +28,7 @@ async function run() {
   const conn = mongoose.createConnection();
   conn.model("User", userSchema);
 
-  const dbURL =
-    process.env.DB_URL ||
-    "mongodb+srv://app:vbSczxBpNmkX5a4q@clustertff.h8qujjg.mongodb.net/?retryWrites=true&w=majority";
+  const dbURL = process.env.DB_URL || "mongodb://localhost:27017";
   // But call `mongoose.connect()`, which connects MongoDB's default
   // connection to MongoDB. `conn` is still disconnected.
   await mongoose.connect(dbURL, { dbName: "tff-project" });
